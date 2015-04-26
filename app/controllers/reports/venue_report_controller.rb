6cb5c1497918dee7cbde9712d37f65b92802506b
Sebastian Siemianowski <sebastian.siemianowski@sage.com>
2015-04-26 05:37:47 +0100

    adds view and controller

---
+++ app/controllers/reports/venue_report_controller.rb
@@ -0,0 +1,8 @@
+class Reports::VenueReportController < ApplicationController
+  def index
+    venue_reporting_service = VenueReportingService.new
+    date_from = DateTime.strptime("04/20/2009 8:00", "%m/%d/%Y %H:%M")
+    date_to = DateTime.strptime("04/20/2020 8:00", "%m/%d/%Y %H:%M")
+    @report = venue_reporting_service.build_report(date_from, date_to)
+  end
+end
